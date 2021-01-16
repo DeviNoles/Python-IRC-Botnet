2120069d815eb5fe52bdc6f0cb47acb6239a5d33
DevinM4 <devin8821@gmail.com>
2021-01-16 15:22:50 -0500

    Commit I am using for YouTube video.

--- Client/client.py
+++ Client/client.py
@@ -9,9 +9,9 @@ def pong(): # respond to server Pings.
     ircsock.send("PONG :pingisn");
 
 def connect():
-    server = "75.90.61.240"; # Enter your Server IP
+    server = "x.x.x.x"; # Enter your Server IP
     channel = "#bot-testing"; # Channel
-    botnick = "B0t-Ubuntu"; # Your bots nick
+    botnick = "B0t"; # Your bots nick
     ircsock.connect((server, 6667)); # Here we connect to the server using the port 6667
 
     sendstr = "USER "+botnick+" "+botnick+" "+botnick+" :Python B0t1.!! Testing Case\r\n";
@@ -19,36 +19,19 @@ def connect():
 
     ircsock.sendall(("NICK "+ botnick +"\r\n").encode('utf-8')) # assign the nick to the bot
 
-    while 1: #this while block is for ping-pong IRC responses (NOT IMPORTANT BUT READ THE IRC STANDARD TO UNDERSTAND THE PORT)
+    while 1: #this while block is for ping-pong IRC responses (NOT IMPORTANT BUT READ THE IRC PROTOCOL TO UNDERSTAND THE PORT)
         ircmsg = ircsock.recv(2048).decode("utf-8")
         print (ircmsg);
         if ircmsg.find ( 'PING' ) != -1:
             ircsock.sendall(( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' ).encode('utf-8'))
             last_ping = time()
             break;
-#    joinchan('#bot-testing')
-
-"""
-    ircsock.connect((server, 6667)); # Here we connect to the server using the port 6667
-    ircmsg = ircsock.recv(2048).decode("utf-8")
-    if ircmsg.find ( 'PING' ) != -1:
-        ircsock.send ( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' )
-        last_ping = time()
-
-    print (ircmsg);
-    sendstr = "USER "+botnick+" "+botnick+" "+botnick+" :Python B0t1.!! Testing Case\r\n";
-    ircsock.send(sendstr);
-
-    ircsock.send("NICK "+ botnick +"\r\n") # assign the nick to the bot
-    joinchan('#bot-testing')
-"""
 
 def joinchan(chan): # join channel(s).
     print("JOINING")
     ircsock.sendall(("JOIN "+chan+"\r\n").encode('utf-8'))
     ircmsg = ""
     while 1:
-    #    pong()
         ircmsg = ircsock.recv(2048).decode("utf-8")
         ircmsg = ircmsg.strip("\n\r")
         print(ircmsg)
